18e7019c4f167a7061357fe734efaa7ce44a9b9b
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-10-25 21:55:05 +0530

    ASCII value

---
+++ Basic Program/ASCII value of character.cpp
@@ -0,0 +1,10 @@
+//Program to print ASCII Value of Character
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char c = 'A';
+	cout << "The ASCII value of " << c << " is " << int(c);
+	return 0;
+}
