33ecf0fad3e130d2502f89032316b4ff32a433d5
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-10-26 23:34:29 +0530

    Area and perimeter of rectangle

---
+++ Basic Program/Area and Perimeter of rectangle.cpp
@@ -0,0 +1,33 @@
+
+//Program to find area and perimeter of rectangle
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int areaRectangle(int a, int b)
+{
+
+   int area = a * b;
+
+   return area;
+}
+ int perimeterRectangle(int a, int b)
+{
+
+   int perimeter = 2*(a + b);
+
+   return perimeter;
+}
+int main()
+{
+
+  int a = 5;
+
+  int b = 6;
+
+  cout << "Area = " <<areaRectangle(a, b) << endl;
+
+  cout << "Perimeter = " <<perimeterRectangle(a, b);
+  getch();
+  return 0;
+} 
