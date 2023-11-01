6c46dbda2d6e23ae189853f0c72f220334e24bfc
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-11-01 23:17:49 +0530

    Looping statement

---
+++ Basic Program/Control_Statement/01For loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int i=0;
+    for(i=0; i<10; i++)
+        cout<<"Hello, World!\n";
+    cout<<endl;
+    getch();
+    return 0;
+}
