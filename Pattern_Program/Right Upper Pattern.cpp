6a3f0f9b624a105a2da9a0e53a58983f145ef374
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-11-03 23:52:28 +0530

    Pattern Printing

---
+++ Pattern_Program/Right Upper Pattern.cpp
@@ -0,0 +1,31 @@
+///Program for Right upper pattern.
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int j=0, i=0, s=0;
+
+    cout<<"\nEnter size for pattern :";
+    cin>>s;
+    cout<<endl;
+
+    for(i=1 ; i<=s ; i++)
+    {
+        for(j=1 ; j<=s ; j++)
+        {
+            if(i==j||i<=j)
+            {
+                cout<<" * ";
+            }
+            else
+            {
+                cout<<"   ";
+            }
+        }
+        cout<<"\n";
+    }
+    getch();
+    return 0;
+}
