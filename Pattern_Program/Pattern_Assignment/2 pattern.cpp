9cc331fdeedaa0caac087d807608c1679459691b
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-11-07 23:46:19 +0530

    Assignment

---
+++ Pattern_Program/Pattern_Assignment/2 pattern.cpp
@@ -0,0 +1,32 @@
+///Program for print character using Left lower pattern.
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int j=0, i=0, s=0;
+    char alp='\0';
+
+    cout<<"\nEnter size for pattern :";
+    cin>>s;
+    cout<<endl;
+    alp='A';
+    for(i=1 ; i<=s ; i++)
+    {
+        for(j=1 ; j<=s ; j++,alp++)
+        {
+            if(i==j||i>=j)
+            {
+                cout<<" "<<alp<<" ";
+            }
+            else
+            {
+                cout<<"   ";
+            }
+        }
+        cout<<"\n";
+    }
+    getch();
+    return 0;
+}
