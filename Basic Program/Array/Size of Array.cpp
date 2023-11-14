3fd511aa2056b9e873dad5c4a9239622d9526942
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-11-14 12:58:24 +0530

    Array Program

---
+++ Basic Program/Array/Size of Array.cpp
@@ -0,0 +1,13 @@
+
+    #include<iostream>
+    #include<conio.h>
+    using namespace std;
+    int main()
+    {
+        int Num[5] = { 23, -47, 0, 41, -39};
+
+        cout<<"\n\t"<<sizeof(Num);
+
+        getch();
+        return 0;
+    }
