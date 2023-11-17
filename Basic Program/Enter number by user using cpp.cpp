231e286af14a01ca0c4473de886d681c556b9a1e
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-11-17 23:10:51 +0530

    Basic Program

---
+++ Basic Program/Enter number by user using cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "\n\nYou entered " << number;    
+    return 0;
+}
