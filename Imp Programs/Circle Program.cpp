4a51171d8bb221eeb355f63c052766dbaab7868e
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-12-19 18:38:16 +0530

    Constructor in CPP

---
+++ Imp Programs/Circle Program.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Circle
+{
+  public:
+    float Rad, Area, Circum;
+    
+    Circle();
+    Circle(float R);
+    Circle(Circle &);
+    
+    void Area_circle();
+    void Circumference_circle();
+    void Display_details();
+    
+    ~Circle();
+    
+  private:
+    float Pi;
+};
+Circle::Circle()
+{
+  Rad=Area=Circum=0;
+  cout<<"\nInside default constructor"<<endl;
+}
+Circle::Circle( float R)
+{
+  
+  cout<<"\nInside parameterised constructor";
+  Rad=R;
+  Area_circle();
+  Circumference_circle();
+}
+Circle::Circle(Circle &t)
+{
+  cout<<"\nInside Copy constructor";
+  Rad=t.Rad;
+  Area=t.Area;
+  Circum=t.Circum;
+  
+}
+void Circle::Area_circle()
+{
+  Pi=3.14;
+  Area=Pi*Rad*Rad;
+}
+void Circle::Circumference_circle()
+{
+  Pi=3.14;
+  Circum=2*Pi*Rad;
+  
+}
+void Circle::Display_details()
+{
+  cout<<"\nArea is "<<Area;
+  cout<<"\nCircumference is "<<Circum<<endl;
+}
+Circle::~Circle()
+{
+  cout<<"\nDestructor is called "<<Rad;
+}
+
+int main()
+{
+  cout<<"\n---- Default Constructor----"<<endl;
+  Circle C1;
+  getch();
+  cout<<"\n---- Parameterized Constructor----"<<endl;
+  Circle C2(5);
+  C2.Display_details();
+  getch();
+  cout<<"\n---- Copy Constructor----"<<endl;
+  Circle C3(C1);
+  C3.Area_circle();
+  C3.Circumference_circle();
+  C3.Display_details();
+}
